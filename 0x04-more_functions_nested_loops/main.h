99dbebfdcc6dfb4f33dcc03f28008ee4faa53b2b
FRANKD-UX <frankdafrica@gmail.com>
2024-07-02 09:50:03 -0700

    0 task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,18 @@
-Main.h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+#include "main.h"
+
+/*
+ * Function prototypes
+ */
 int _isupper(int c);
-int _isdigit(int c);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-int mul(int a, int b);
-void print_number(int n);
+void someFunction(void);
+
+/*
+ * Constants or macros
+ */
+#define MAX_SIZE 100
+
+#endif /* MAIN_H */
 
