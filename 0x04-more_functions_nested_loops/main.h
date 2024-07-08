ac7dbdb49e5b5c8ef06bb168042cc41c5559cd75
FRANKD-UX <frankdafrica@gmail.com>
2024-07-07 17:20:58 -0700

    6

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,6 +12,7 @@ int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 /*
  * Constants or macros
@@ -19,4 +20,5 @@ void more_numbers(void);
 #define MAX_SIZE 100
 
 #endif /* MAIN_H */
+
 
