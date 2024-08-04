3969f9f55d1880630b855d8ff0702c4b7708f680
FRANKD-UX <frankdafrica@gmail.com>
2024-08-04 12:10:04 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Description: This function compares the two strings s1 and s2.
+ * It returns an integer less than, equal to, or greater than zero,
+ * if s1 is found to be less than, equal to, or greater than s2, respectively.
+ *
+ * Return: An integer less than, equal to, or greater than zero.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+
+	return (s1[i] - s2[i]);
+}
+
