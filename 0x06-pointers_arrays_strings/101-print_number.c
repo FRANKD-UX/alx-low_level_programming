ea32c74b138a1f1bebb3b9b14a7951241fa67a6e
FRANKD-UX <frankdafrica@gmail.com>
2024-09-05 00:44:09 -0700

    1

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: the integer to be printed.
+ * print_number - prints an integer
+ * @n: integer to print
  */
 void print_number(int n)
 {
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n; /* Make the number positive */
+		n = -n;
 	}
 
-	if (n / 10 != 0)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
-	_putchar((n % 10) + '0');
+
+	_putchar(n % 10 + '0');
 }
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *string_toupper(char *str);
 char *cap_string(char *);
 char *leet(char *s);
 char *rot13(char *);
-
+int _putchar(char c);
+void print_number(int n);
 #endif /* MAIN_H */
 
