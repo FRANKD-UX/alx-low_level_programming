1f780798508bacac5074f5a48a9a0cb23da63c10
FRANKD-UX <frankdafrica@gmail.com>
2024-09-05 12:41:04 -0700

    1

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,18 +1,31 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
-  * set_string - main function
-  *
-  * @s: Function parameter
-  *
-  * @to: Function parameter
-  *
-  * Return: Always 0.
-  */
-void set_string(char **s, char *to)
-
+ * set_string - sets the value of a pointer to a new string
+ * @s: double pointer to the string to be updated
+ * @to: the new string to copy
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, const char *to)
 {
-	*s = to;
+	char *new_string;
 
+	if (*s != NULL)
+	{
+		free(*s);
+	}
+
+	new_string = malloc(strlen(to) + 1);
+	if (new_string == NULL)
+	{
+		*s = NULL;
+		return;
+	}
+
+	strcpy(new_string, to);
+	*s = new_string;
 }
 
