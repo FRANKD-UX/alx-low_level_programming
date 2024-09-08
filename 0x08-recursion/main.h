6121a90132da0e415ff0016c4de7d93d7915bc22
FRANKD-UX <frankdafrica@gmail.com>
2024-09-08 07:40:12 -0700

    a

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,10 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n); /* Declare the factorial function */
 int _pow_recursion(int x, int y);  /* Add this prototype */
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_prime_helper(int n, int i); /* Prototype for prime helper function */
+int _sqrt_recursion(int n);
 
 #endif
 
