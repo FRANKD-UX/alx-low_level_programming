cf418d1ec86f48f2e7487c2b3ff7e5935a6519b8
FRANKD-UX <frankdafrica@gmail.com>
2024-09-13 19:10:57 -0700

    k

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: This struct defines a dog's basic information.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
+
